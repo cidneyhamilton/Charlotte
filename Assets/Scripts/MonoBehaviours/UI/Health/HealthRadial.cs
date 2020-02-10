3692bc2cda2d01919148d207fcea5864f6bd3324
Cidney Hamilton <cidney@cidneyhamilton.com>
2020-02-10 11:06:51 -0500

    Add a Dragon Age-style health radial to the hero.

---
+++ Assets/Scripts/MonoBehaviours/UI/Health/HealthRadial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Charlotte {
+
+    public class HealthRadial : HealthView
+    {
+
+	// TODO: Define in one place
+	const int maxHealth = 10;	
+
+	protected override void UpdateHealth(int currentHealth) {
+	    float zAngle = 180f * currentHealth / maxHealth;
+	    transform.Rotate(0, 0, zAngle);
+	}
+    }
+}
